de0b6e5a252aed0a213468ed956cfc14a473dd42
rodrigondec <rodrigondec@gmail.com>
2020-12-09 18:19:45 -0300

    adiciona BaseBBWrapper

---
+++ imobanco_bb/constants.py
@@ -0,0 +1,11 @@
+from decouple import config
+
+
+IS_SANDBOX = config("IMOBANCO_BB_IS_SANDBOX", default=True)
+"""É ambiente de teste?"""
+
+BASIC_TOKEN = config("IMOBANCO_BB_BASIC_TOKEN", default=True)
+"""Token básico para autenticação"""
+
+GW_APP_KEY = config("IMOBANCO_BB_GW_APP_KEY", default=True)
+"""developer_application_key"""
