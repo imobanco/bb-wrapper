220ebb0efd911391dda8fba637cbd0e11f74ce14
rodrigondec <rodrigondec@gmail.com>
2020-12-20 23:23:05 -0300

    altera exemplos

--- examples/cobranças/registra_boleto_3.py
+++ examples/cobranças/registra_boleto_3.py
@@ -6,43 +6,35 @@ from bb_wrapper.wrapper.cobrancas import CobrancasBBWrapper
 
 wrapper = CobrancasBBWrapper()
 
-data = {
-    "numeroConvenio": 3128557,
-    "numeroCarteira": 17,
-    "numeroVariacaoCarteira": 35,
-    "codigoModalidade": 1,
-    "dataEmissao": "07.12.2020",
-    "dataVencimento": "07.12.2020",
-    "valorOriginal": 100.00,
-    "quantidadeDiasProtesto": 0,
-    "indicadorNumeroDiasLimiteRecebimento": "N",
-    "numeroDiasLimiteRecebimento": 0,
-    "codigoAceite": "N",
-    "codigoTipoTitulo": 4,
-    "descricaoTipoTitulo": "DS",
-    "indicadorPermissaoRecebimentoParcial": "N",
-    "numeroTituloBeneficiario": "TESTE2",
-    "textoCampoUtilizacaoBeneficiario": "TESTE3",
-    "numeroTituloCliente": "00031285571231230011",
-    "textoMensagemBloquetoOcorrencia": "TESTE5",
-    "pagador": {
-        "tipoRegistro": 1,
-        "numeroRegistro": "71128590182",
-        "nome": "NOME",
-        "endereco": "ENDERECO",
-        "cep": "70675727",
-        "cidade": "SAO PAULO",
-        "bairro": "CENTRO",
-        "uf": "SP",
-        "telefone": "999939669",
-    },
-    "avalista": {
-        "tipoRegistro": 1,
-        "numeroRegistro": "71128590182",
-        "nomeRegistro": "NOME",
-    },
-    "jurosMora": {"tipo": 1, "valor": 0.1},
-}
+data = wrapper.create_boleto_data_with_defaults(
+    {
+        "dataEmissao": "07.12.2020",
+        "dataVencimento": "07.12.2020",
+        "valorOriginal": 100.00,
+        "descricaoTipoTitulo": "DS",
+        "numeroTituloBeneficiario": "TESTE2",
+        "textoCampoUtilizacaoBeneficiario": "TESTE3",
+        "numeroTituloCliente": "00031285571231230011",
+        "textoMensagemBloquetoOcorrencia": "TESTE5",
+        "pagador": {
+            "tipoRegistro": 1,
+            "numeroRegistro": "71128590182",
+            "nome": "NOME",
+            "endereco": "ENDERECO",
+            "cep": "70675727",
+            "cidade": "SAO PAULO",
+            "bairro": "CENTRO",
+            "uf": "SP",
+            "telefone": "999939669",
+        },
+        "avalista": {
+            "tipoRegistro": 1,
+            "numeroRegistro": "71128590182",
+            "nomeRegistro": "NOME",
+        },
+        "jurosMora": {"tipo": 1, "valor": 0.1},
+    }
+)
 
 response = wrapper.registra_boleto(data)
 
