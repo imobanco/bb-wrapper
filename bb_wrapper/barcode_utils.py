49124717bd27956b28285fa5d84225d35b268dd0
rodrigondec <rodrigondec@gmail.com>
2020-12-20 23:21:23 -0300

    fmt

--- bb_wrapper/barcode_utils.py
+++ bb_wrapper/barcode_utils.py
@@ -11,6 +11,6 @@ def generate_barcode_b64image(barcode_number, filepath=None):
     barcode_generator.write(buffer)
     base64_img = base64.b64encode(buffer.getvalue())
     if filepath is not None:
-        with open(filepath, 'wb') as f:
+        with open(filepath, "wb") as f:
             barcode_generator.write(f)
     return base64_img
