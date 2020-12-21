cca612840003688f99b407853da66d85869da5b3
rodrigondec <rodrigondec@gmail.com>
2020-12-21 00:01:29 -0300

    remove ImageWriter e utiliza o `generateP` da lib!

--- bb_wrapper/barcode_utils.py
+++ bb_wrapper/barcode_utils.py
@@ -1,16 +1,11 @@
 import io
 import base64
 
-from barcode import ITF
-from barcode.writer import ImageWriter
+from barcode import generate
 
 
-def generate_barcode_b64image(barcode_number, filepath=None):
-    barcode_generator = ITF(barcode_number, writer=ImageWriter())
+def generate_barcode_b64image(barcode_number, text=None):
     buffer = io.BytesIO()
-    barcode_generator.write(buffer)
+    generate(name='itf', code=barcode_number, output=buffer, text=text)
     base64_img = base64.b64encode(buffer.getvalue())
-    if filepath is not None:
-        with open(filepath, "wb") as f:
-            barcode_generator.write(f)
     return base64_img
