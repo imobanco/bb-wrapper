f3464e4a8eef46aa04cc437dd6be6e2e35831902
rodrigondec <rodrigondec@gmail.com>
2020-12-20 23:22:22 -0300

    ajusta constantes

--- bb_wrapper/constants.py
+++ bb_wrapper/constants.py
@@ -4,11 +4,17 @@ from decouple import config
 IS_SANDBOX = config("IMOBANCO_BB_IS_SANDBOX", default=True, cast=bool)
 """É ambiente de teste?"""
 
-BASIC_TOKEN = config("IMOBANCO_BB_BASIC_TOKEN", default=True)
+BASIC_TOKEN = config("IMOBANCO_BB_BASIC_TOKEN", default="")
 """Token básico para autenticação"""
 
-GW_APP_KEY = config("IMOBANCO_BB_GW_APP_KEY", default=True)
+GW_APP_KEY = config("IMOBANCO_BB_GW_APP_KEY", default="")
 """developer_application_key"""
 
-CONVENIO_NUMBER = config("IMOBANCO_BB_CONVENIO_NUMBER", default=True)
+CONVENIO = config("IMOBANCO_BB_CONVENIO", default="")
 """número do convênio"""
+
+CARTEIRA = config("IMOBANCO_BB_CARTEIRA", default="")
+"""número da carteira"""
+
+VARIACAO_CARTEIRA = config("IMOBANCO_BB_VARIACAO_CARTEIRA", default="")
+"""número da variação da carteira"""
