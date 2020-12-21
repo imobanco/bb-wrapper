1534f867e2d92cd24eaa7312b4e311fc777f8b93
rodrigondec <rodrigondec@gmail.com>
2020-12-20 22:24:50 -0300

    altera writer para ImageWriter

--- bb_wrapper/barcode_utils.py
+++ bb_wrapper/barcode_utils.py
@@ -2,11 +2,15 @@ import io
 import base64
 
 from barcode import ITF
+from barcode.writer import ImageWriter
 
 
-def generate_barcode_b64image(barcode_number):
-    barcode_generator = ITF(barcode_number)
+def generate_barcode_b64image(barcode_number, filepath=None):
+    barcode_generator = ITF(barcode_number, writer=ImageWriter())
     buffer = io.BytesIO()
     barcode_generator.write(buffer)
     base64_img = base64.b64encode(buffer.getvalue())
+    if filepath is not None:
+        with open(filepath, 'wb') as f:
+            barcode_generator.write(f)
     return base64_img
