bdc5dfeca88e0efc31daf53d49c9172dbc007943
rodrigondec <rodrigondec@gmail.com>
2021-01-08 09:12:50 -0300

    altera exemplos

--- examples/cobranças/registra_boleto_1.py
+++ examples/cobranças/registra_boleto_1.py
@@ -16,17 +16,16 @@ data = wrapper.create_boleto_data_with_defaults(
         "numeroTituloBeneficiario": number,
         "numeroTituloCliente": wrapper.build_our_number(number),
         "pagador": {
-            "tipoRegistro": 1,
-            "numeroRegistro": "71128590182",
-            "nome": "Nome' do João da Mária",
-            "endereco": "Rua Joazeirão da Sílva 1º",
-            "cep": "70675727",
-            "cidade": "São Paulo",
-            "bairro": "Centro",
-            "uf": "SP",
-            "telefone": "999939669",
-        },
-        "email": "cliente@email.com",
+            "tipoInscricao": 1,
+            "numeroInscricao": "71128590182",
+            "nome": "Nome",
+            "endereco": "Rua Prudente de morais",
+            "cep": "59150000",
+            "cidade": "Natal",
+            "bairro": "Tirol",
+            "uf": "RN",
+            "telefone": "",
+        }
     }
 )
 
