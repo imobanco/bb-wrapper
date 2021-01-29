b888b153e6067f14840a61b1594605ab578e4b35
rodrigondec <rodrigondec@gmail.com>
2021-01-29 14:42:22 -0300

    corrige campo de indicador

--- bb_wrapper/wrapper/cobrancas.py
+++ bb_wrapper/wrapper/cobrancas.py
@@ -125,7 +125,8 @@ class CobrancasBBWrapper(BaseBBWrapper):
             "numeroVariacaoCarteira": self.__variacao_carteira,
             "codigoModalidade": 1,  # SIMPLES (4 seria split!)
             "quantidadeDiasProtesto": 0,  # 0 dias para protestar!
-            "numeroDiasLimiteRecebimento": 0,  # SEM tolerância
+            "indicadorAceiteTituloVencido": 'S',  # COM tolerância
+            "numeroDiasLimiteRecebimento": 0,  # 0 dias de tolerância
             "codigoAceite": "N",  # Boleto não reconhecido pelo pagador
             "codigoTipoTitulo": 4,  # convênio tipo 4 (cliente numera, emite e expede)
             "indicadorPermissaoRecebimentoParcial": "N",  # sem recibimento parcial!
--- examples/cobranças/registra_boleto_1.py
+++ examples/cobranças/registra_boleto_1.py
@@ -7,8 +7,7 @@ from bb_wrapper.wrapper.cobrancas import CobrancasBBWrapper
 
 wrapper = CobrancasBBWrapper()
 
-number = 9999999986
-
+number = 9999999999
 today = date.today()
 bb_fmt = "%d.%m.%Y"
 
