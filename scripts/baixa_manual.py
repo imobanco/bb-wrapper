f9b3e16e997aadc2e07fe1800e21ee4b426ce5f2
rodrigondec <rodrigondec@gmail.com>
2021-05-24 14:19:34 -0300

    altera script de baixa manual

--- scripts/baixa_manual.py
+++ scripts/baixa_manual.py
@@ -1,13 +1,30 @@
+from threading import Thread
+
 from bb_wrapper.wrapper.cobrancas import CobrancasBBWrapper
 
 
 wrapper = CobrancasBBWrapper()
 
 
-for number in range(9999999999):
-    print(f"Dando baixa no {number}...")
-    try:
-        response = wrapper.baixa_boleto(wrapper.build_our_number(number))
-        print(response)
-    except Exception as e:
-        print(e)
+number_of_threads = 5
+threads = []
+
+
+def baixa_boletos(index):
+    for number in range(index, 9999999999, number_of_threads):
+        print(f"Dando baixa no {number}...")
+        try:
+            response = wrapper.baixa_boleto(wrapper.build_our_number(number))
+            print(number, response)
+        except Exception as e:
+            print(number, e)
+
+
+for i in range(number_of_threads):
+    start_index = i + 0
+    t = Thread(target=baixa_boletos, args=[start_index])
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
