5b42940c533bc8c44ce3e94542ba97622ed2d182
rodrigondec <rodrigondec@gmail.com>
2021-07-14 15:05:46 -0300

    fmt

--- examples/pix_cob/devolver_pix.py
+++ examples/pix_cob/devolver_pix.py
@@ -7,7 +7,7 @@ from bb_wrapper.wrapper import PIXCobBBWrapper
 c = PIXCobBBWrapper()
 
 end_to_end_id = "E00000000202101081802KEP4RED55RB"
-txid = '?'
+txid = "?"
 
 response = c.devolver_pix(end_to_end_id, "0.01", txid)
 
