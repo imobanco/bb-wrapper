c781ba518e3835ca7964f765f640d19eeeb16d80
rodrigondec <rodrigondec@gmail.com>
2021-08-23 17:15:01 -0300

    documenta melhor

--- bb_wrapper/services/febrabran.py
+++ bb_wrapper/services/febrabran.py
@@ -10,6 +10,10 @@ class FebrabranService:
             - página 14 https://github.com/imobanco/bb-wrapper/blob/7643255ac3d6f4ed1d6086cc2ad37c281659ea95/docs/Layout%20-%20C%C3%B3digo%20de%20Barras%20ATUALIZADO.pdf  # noqa
         """
         dv = ModService().mod_10(number)
+
+        """
+        quando o resto da divisão for 0 (zero), o DAC calculado é o 0 (zero).
+        """
         if dv == 10:
             dv = 0
         return dv
--- bb_wrapper/services/mod.py
+++ bb_wrapper/services/mod.py
@@ -3,6 +3,13 @@ class ModService:
         """
         Método para calcular o DV módulo 10.
 
+        O DAC (Dígito de Auto-Conferência) módulo 10, de um número é calculado multiplicando
+        cada algarismo, pela seqüência de multiplicadores 2, 1, 2, 1, ...
+        posicionados da direita para a esquerda.
+
+        A soma dos algarismos do produto é dividida por 10 e o DAC será a diferença entre o
+        divisor (10) e o resto da divisão.
+
         Referências:
             - https://pt.wikipedia.org/wiki/D%C3%ADgito_verificador
             - https://github.com/eduardocereto/pyboleto/blob/1fed215eac2c974efc6f03a16b94406c2bb55cc2/pyboleto/data.py#L453  # noqa
@@ -14,12 +21,34 @@ class ModService:
             raise TypeError("O número deve estar no formato de str!")
 
         total = 0
+
+        """
+        O fator começa com 2
+        """
         factor = 2
-        for digit in reversed(num):
+
+        """
+        O produto é feito da direita para esquerda!
+        """
+        reversed_num = reversed(num)
+
+        for digit in reversed_num:
             result = int(digit) * factor
+
+            """
+            A soma dos algarismos do produto será dividida por 10...
+            """
             total += sum([int(d) for d in str(result)])
+
+            """
+            O fator alterna entre 2 e 1...
+            """
             factor = (factor % 2) + 1
 
+        """
+        o DAC será a diferença entre o
+        divisor (10) e o resto da divisão.
+        """
         rest = total % 10
         dv = 10 - rest
         return dv
