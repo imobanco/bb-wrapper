b397a067fea27348cbd09781e0ef70d0791db565
rodrigondec <rodrigondec@gmail.com>
2021-08-25 13:22:09 -0300

    altera testes

--- tests/services/test_mod.py
+++ tests/services/test_mod.py
@@ -4,206 +4,37 @@ from bb_wrapper.services import ModService
 
 
 class ModServiceTestCase(TestCase):
-    def test_mod_10_1(self):
-        """
-        Dado:
-            - um número "1"
-        Quando:
-            - for chamado ModService().mod_10(number)
-        Então:
-            - o resultado deve ser 8
-        """
-        number = "1"
+    def test_mod_10(self):
+        """"""
+        number_to_mod = {
+            "1": "8",
+            "2": "6",
+            "32": "3",
+            "21": "6",
+            "46": "3",
+            "79": "4",
+            "51234": "3",
+        }
 
-        result = ModService().mod_10(number)
+        for number, mod in number_to_mod.items():
+            with self.subTest(number):
+                result = ModService().mod_10(number)
 
-        expected = 8
+                self.assertEqual(result, mod)
 
-        self.assertEqual(result, expected)
+    def test_mod_11(self):
+        """"""
+        number_to_mod = {
+            "1": "9",
+            "2": "7",
+            "21": "3",
+            "32": "9",
+            "46": "9",
+            "79": "5",
+            "51234": "6",
+        }
+        for number, mod in number_to_mod.items():
+            with self.subTest(number):
+                result = ModService().mod_11(number)
 
-    def test_mod_10_2(self):
-        """
-        Dado:
-            - um número "2"
-        Quando:
-            - for chamado ModService().mod_10(number)
-        Então:
-            - o resultado deve ser 6
-        """
-        number = "2"
-
-        result = ModService().mod_10(number)
-
-        expected = 6
-
-        self.assertEqual(result, expected)
-
-    def test_mod_10_3(self):
-        """
-        Dado:
-            - um número "12"
-        Quando:
-            - for chamado ModService().mod_10(number)
-        Então:
-            - o resultado deve ser 5
-        """
-        number = "12"
-
-        result = ModService().mod_10(number)
-
-        expected = 5
-
-        self.assertEqual(result, expected)
-
-    def test_mod_10_4(self):
-        """
-        Dado:
-            - um número "21"
-        Quando:
-            - for chamado ModService().mod_10(number)
-        Então:
-            - o resultado deve ser 6
-        """
-        number = "21"
-
-        result = ModService().mod_10(number)
-
-        expected = 6
-
-        self.assertEqual(result, expected)
-
-    def test_mod_11_1(self):
-        """
-        Dado:
-            - um número "1"
-        Quando:
-            - for chamado ModService().mod_11(number)
-        Então:
-            - o resultado deve ser 2
-        """
-        number = "1"
-
-        result = ModService().mod_11(number)
-
-        expected = 2
-
-        self.assertEqual(result, expected)
-
-    def test_mod_11_2(self):
-        """
-        Dado:
-            - um número "2"
-        Quando:
-            - for chamado ModService().mod_11(number)
-        Então:
-            - o resultado deve ser 7
-        """
-        number = "2"
-
-        result = ModService().mod_11(number)
-
-        expected = 4
-
-        self.assertEqual(result, expected)
-
-    def test_mod_11_3(self):
-        """
-        Dado:
-            - um número "12"
-        Quando:
-            - for chamado ModService().mod_11(number)
-        Então:
-            - o resultado deve ser 7
-        """
-        number = "12"
-
-        result = ModService().mod_11(number)
-
-        expected = 7
-
-        self.assertEqual(result, expected)
-
-    def test_mod_11_4(self):
-        """
-        Dado:
-            - um número "21"
-        Quando:
-            - for chamado ModService().mod_11(number)
-        Então:
-            - o resultado deve ser 8
-        """
-        number = "21"
-
-        result = ModService().mod_11(number)
-
-        expected = 8
-
-        self.assertEqual(result, expected)
-
-    def test_mod_11_5(self):
-        """
-        Dado:
-            - um número "1"
-        Quando:
-            - for chamado ModService().mod_11(number, r_base=1)
-        Então:
-            - o resultado deve ser 9
-        """
-        number = "1"
-
-        result = ModService().mod_11(number, False)
-
-        expected = 9
-
-        self.assertEqual(result, expected)
-
-    def test_mod_11_6(self):
-        """
-        Dado:
-            - um número "2"
-        Quando:
-            - for chamado ModService().mod_11(number, r_base=1)
-        Então:
-            - o resultado deve ser 7
-        """
-        number = "2"
-
-        result = ModService().mod_11(number, False)
-
-        expected = 7
-
-        self.assertEqual(result, expected)
-
-    def test_mod_11_7(self):
-        """
-        Dado:
-            - um número "12"
-        Quando:
-            - for chamado ModService().mod_11(number, r_base=1)
-        Então:
-            - o resultado deve ser 4
-        """
-        number = "12"
-
-        result = ModService().mod_11(number, False)
-
-        expected = 4
-
-        self.assertEqual(result, expected)
-
-    def test_mod_11_8(self):
-        """
-        Dado:
-            - um número "21"
-        Quando:
-            - for chamado ModService().mod_11(number, r_base=1)
-        Então:
-            - o resultado deve ser 3
-        """
-        number = "21"
-
-        result = ModService().mod_11(number, False)
-
-        expected = 3
-
-        self.assertEqual(result, expected)
+                self.assertEqual(result, mod)
