fd8a343411e696c9507e14eab9f191ad467529bf
rodrigondec <rodrigondec@gmail.com>
2021-08-25 18:29:45 -0300

    remove arquivos obsoletos

--- poetry2nix.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.poetry2nix.mkPoetryApplication {
-  poetrylock = ./poetry.lock;
-  pyproject = ./pyproject.toml;
-  python = pkgs.python3;
-  src = pkgs.lib.cleanSource ./.;
-}
