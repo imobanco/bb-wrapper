940ff8de60db4569fbb343d3ff4ac4eedf81bfc3
rodrigondec <rodrigondec@gmail.com>
2022-02-11 17:41:46 -0300

    adiciona exemplos

--- examples/lotes_pagamento/listar_pagamentos.py
+++ examples/lotes_pagamento/listar_pagamentos.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 
 from examples.utils import dump_response
 
@@ -6,9 +7,12 @@ from bb_wrapper.wrapper import PagamentoLoteBBWrapper
 
 c = PagamentoLoteBBWrapper(cert=("./certs/cert.pem", "./certs/key.pem"))
 
-number = "579175"
+inicio = date(2022, 2, 8)
+fim = date(2022, 2, 11)
+bb_fmt = "%d%m%Y"
 
-
-response = c.liberar_pagamentos(number)
+response = c.listar_pagamentos(
+    inicio=inicio.strftime(bb_fmt), fim=fim.strftime(bb_fmt), index=0
+)
 
 dump_response(response, os.path.realpath(__file__))
--- examples/lotes_pagamento/resgatar_lote.py
+++ examples/lotes_pagamento/resgatar_lote.py
@@ -6,9 +6,11 @@ from bb_wrapper.wrapper import PagamentoLoteBBWrapper
 
 c = PagamentoLoteBBWrapper(cert=("./certs/cert.pem", "./certs/key.pem"))
 
-number = "579175"
+_id = "579175"
 
 
-response = c.liberar_pagamentos(number)
+response = c.resgatar_lote(
+    _id,
+)
 
 dump_response(response, os.path.realpath(__file__))
