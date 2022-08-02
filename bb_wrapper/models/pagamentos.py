7de85e6c685512ca251faf0e36fe198730cc6996
Rodrigo Castro <rodrigondec@gmail.com>
2022-08-02 14:32:27 -0300

    Update bb_wrapper/models/pagamentos.py
    Co-authored-by: Breno Viana <bmfviana@gmail.com>

--- bb_wrapper/models/pagamentos.py
+++ bb_wrapper/models/pagamentos.py
@@ -212,7 +212,7 @@ ERROS_VALIDACAO_PAGAMENTO_DICT = {
     42: "Número de inscrição do avalista não é numérico. Informar números.",
     43: "Dígito do CPF para o pagador inválido. Verifique o nº correto.",
     44: "Dígito do CPF para o beneficiário inválido. Verifique o número correto.",
-    45: "Dígito do CPF para o avalista inválido. Verifique o nº correto.",
+    45: "Dígito do CPF para o avalista inválido. Verifique o número correto.",
     46: "Dígito do CNPJ para o pagador inválido. Verifique o número correto.",
     47: "Dígito do CNPJ para o beneficiário inválido. Verifique o número correto.",
     48: "Dígito do CNPJ para o avalista inválido. Verifique o número correto.",
