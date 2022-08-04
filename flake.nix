9a1e965b6338d5a6fcfa81e4e8f644c2926f7194
Breno Viana <bmfviana@gmail.com>
2022-08-04 13:43:56 -0300

    Adiciona flake.nix

--- flake.nix
+++ flake.nix
@@ -33,6 +33,14 @@
             # devo abrir issue no github do nixpkgs
             export TMPDIR=/tmp
 
+            # O PyCharm ativa por padrão o ambiente virtual.
+            # Esse comando cria o .venv caso não exista e
+            # ativa o .venv.
+            # Notar que pode haver dessincronia por conta de
+            # um .venv desatualizado.
+            test -f .venv/bin/activate || make poetry.install
+            source .venv/bin/activate
+
             echo "Entering the nix devShell no income back"
           '';
         };
