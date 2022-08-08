6646e82bb957a33ed4336ef0606495e5796c8e7e
Breno Viana <bmfviana@gmail.com>
2022-08-08 14:58:15 -0300

    implementa testes de autenticação

--- tests/wrapper/test_bb_base.py
+++ tests/wrapper/test_bb_base.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import patch
 
 from tests.utils import IsolatedEnvTestCase, MockedBBTestCase
 from bb_wrapper.wrapper.bb import BaseBBWrapper
@@ -6,19 +6,50 @@ from bb_wrapper.wrapper.bb import BaseBBWrapper
 
 class BaseBBWrapperTestCase(IsolatedEnvTestCase, MockedBBTestCase):
     def test_authenticate(self):
-        self.mocked_auth_requests.post.return_value = self.build_response_mock(
-            401, data={"access_token": "access_token1", "token_type": "token_type"}
-        )
-        with patch('bb_wrapper.wrapper.bb.BaseBBWrapper.time.time') as mocked_time:
-            mocked_time.return_value = 1
+        """
+        Teste para verificar a validação da autenticação.
+
+        Dado:
+            -
+        Quando:
+            - for chamado BaseBBWrapper()._BaseBBWrapper__authenticate()
+        Então:
+            - o resultado da autenticação deve ser True
+        """
         result = BaseBBWrapper()._BaseBBWrapper__authenticate()
+        self.assertTrue(result)
 
-        self.assertEqual(result, True)
+    def test_reauthentication(self):
+        """
+        Teste para verificar a validação da reautenticação.
+
+        Dado:
+            -
+        Quando:
+            - o token de autenticação expirar
+        Então:
+            - uma nova autenticação deve ser realizada
+            - o wrapper deve ter um novo token de autenticação
+        """
+        bb_wrapper = BaseBBWrapper()
 
         self.mocked_auth_requests.post.return_value = self.build_response_mock(
-            401, data={"access_token": "access_token2", "token_type": "token_type"}
+            200, data={"access_token": "access_token1", "token_type": "token_type"}
         )
+        result = bb_wrapper._BaseBBWrapper__authenticate()
 
-        result = BaseBBWrapper()._BaseBBWrapper__authenticate(force=True)
+        self.assertTrue(result)
+        self.assertEqual(bb_wrapper.data.access_token, "access_token1")
 
-        self.assertEqual(BaseBBWrapper().data.access_token, 'access_token2')
+        with patch("bb_wrapper.wrapper.bb.time.time") as mocked_time:
+            mocked_time.return_value = (
+                bb_wrapper.data.token_time + bb_wrapper.TOKEN_TIME
+            )
+
+        self.mocked_auth_requests.post.return_value = self.build_response_mock(
+            200, data={"access_token": "access_token2", "token_type": "token_type"}
+        )
+        result = bb_wrapper._BaseBBWrapper__authenticate(force_auth=True)
+
+        self.assertTrue(result)
+        self.assertEqual(bb_wrapper.data.access_token, "access_token2")
