a44cc3ca16118de93ed332d0cfc39adb1a72bdf5
rodrigondec <rodrigondec@gmail.com>
2023-09-14 11:07:24 -0300

    ajustes

--- bb_wrapper/services/b64.py
+++ bb_wrapper/services/b64.py
@@ -6,9 +6,9 @@ class Base64Service:
     PNG_DATA_URI_SCHEMA = "data:image/png;base64"
     URI_MAPPING = {"png": PNG_DATA_URI_SCHEMA, "svg": SVG_DATA_URI_SCHEMA}
 
-    def _generate_b64_image_bytes(self, bytes_content, return_as_string=False):
+    def _generate_b64image_bytes(self, bytes_content, return_as_string=False):
         """
-        Método auxiliar
+        Método auxiliar para codificar como base 64 os bytes da imagem
         """
         base64_image_bytes = base64.b64encode(bytes_content)
         if return_as_string:
@@ -16,8 +16,14 @@ class Base64Service:
         return base64_image_bytes
 
     def generate_b64image_string(self, bytes_content, image_type="png", add_uri=True):
+        """
+        Método para gerar uma string base64 de imagem a partir de bytes.
+
+        Note:
+            É possível gerar SVG e PNG de acordo com o image_type
+        """
         uri = self.URI_MAPPING[image_type]
-        base64_string = self._generate_b64_image_bytes(
+        base64_string = self._generate_b64image_bytes(
             bytes_content, return_as_string=True
         )
         if add_uri:
@@ -28,7 +34,10 @@ class Base64Service:
         self, buffer, image_type="png", add_uri=True
     ):
         """
-        Método para gerar uma string base64 de imagem svg a partir de um buffer
+        Método para gerar uma string base64 de imagem a partir de um buffer.
+
+        Note:
+            É possível gerar SVG e PNG de acordo com o image_type
         """
         return self.generate_b64image_string(
             buffer.getvalue(), image_type=image_type, add_uri=add_uri
