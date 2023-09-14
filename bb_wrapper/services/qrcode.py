9a2023b6e94fcefa24bdc0762d1e3d1e726cfec3
rodrigondec <rodrigondec@gmail.com>
2023-09-14 10:42:13 -0300

    altera qrcode

--- bb_wrapper/services/qrcode.py
+++ bb_wrapper/services/qrcode.py
@@ -2,16 +2,28 @@ import io
 
 from qrcode import make as generate_qrcode
 from qrcode.image.svg import SvgImage
+from qrcode.image.pil import PilImage
 
 from .b64 import Base64Service
 
 
 class QRCodeService:
-    def generate_qrcode_b64image(self, qrcode_data):
+    def __init__(self):
+        self.svg_factory = SvgImage
+        self.png_factory = PilImage
+
+        self.factory_mapping = {"png": PilImage, "svg": SvgImage}
+
+    def generate_qrcode_b64image(self, qrcode_data, image_type="png"):
         """
         Método para gerar uma imagem base64 a partir de um qrcode data.
         """
+        factory = self.factory_mapping[image_type]
+
         buffer = io.BytesIO()
-        qrcode = generate_qrcode(qrcode_data, image_factory=SvgImage)
+        qrcode = generate_qrcode(qrcode_data, image_factory=factory)
         qrcode.save(buffer)
-        return Base64Service().generate_b64image_from_buffer(buffer)
+
+        return Base64Service().generate_b64image_string_from_buffer(
+            buffer, image_type=image_type
+        )
