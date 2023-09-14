13b11e925ce5cac4930398d51842151094411a77
rodrigondec <rodrigondec@gmail.com>
2023-09-14 14:50:54 -0300

    adiciona método auxiliar para o qrcode

--- bb_wrapper/services/qrcode.py
+++ bb_wrapper/services/qrcode.py
@@ -12,16 +12,24 @@ class QRCodeService:
     PNG_FACTORY = PilImage
     FACTORY_MAPPING = {"png": PNG_FACTORY, "svg": SVG_FACTORY}
 
-    def generate_qrcode_b64image(self, qrcode_data, image_type="png"):
+    def _generate_qrcode_image_bytes(self, qrcode_data, image_type="png"):
         """
-        Método para gerar uma imagem base64 a partir de um qrcode data.
+        Método para gerar os bytes da imagem a partir de um qrcode data.
         """
         factory = self.FACTORY_MAPPING[image_type]
 
         buffer = io.BytesIO()
         qrcode = generate_qrcode(qrcode_data, image_factory=factory)
         qrcode.save(buffer)
+        return buffer.getvalue()
 
-        return Base64Service().generate_b64image_string_from_buffer(
-            buffer, image_type=image_type
+    def generate_qrcode_b64image(self, qrcode_data, image_type="png"):
+        """
+        Método para gerar uma imagem base64 a partir de um qrcode data.
+        """
+        image_bytes = self._generate_qrcode_image_bytes(
+            qrcode_data=qrcode_data, image_type=image_type
+        )
+        return Base64Service().generate_b64image_string(
+            image_bytes, image_type=image_type
         )
