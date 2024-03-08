c64777701a462a02a3616ca1b1e6147bc3011df1
Breno Viana <brenomfviana@gmail.com>
2024-03-08 14:55:08 -0300

    corrige response reason

--- bb_wrapper/wrapper/request.py
+++ bb_wrapper/wrapper/request.py
@@ -69,8 +69,8 @@ class RequestsWrapper:
             response.data = response.json(strict=False)
         except JSONDecodeError:
             response.data = {}
-        response.reason = response.data
         response_content = response.data if response.data else response.content
+        response.reason = response_content
         request_body = response.request.body
         logger.debug(
             f"Requisição {response.request.method} {response.request.url}\n"
